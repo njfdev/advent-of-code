a5772a388caf40023715642c0a84d1d31eeb6b39
Nicholas Fasching <91689117+njfdev@users.noreply.github.com>
2022-12-17 17:22:42 -0500

    Add Day 17 for 2022

---
+++ 2022/(copied) day-17/README.md
@@ -0,0 +1,15 @@
+# Day 17 - Advent of Code
+
+You can find the link for day 17, 2022 [here](https://adventofcode.com/2022/day/17). This folder will provide complete solutions for part 1 and part 2 of day 17.
+
+NOTE: This code was used from GitHub User [Fadi88](https://github.com/Fadi88/AoC/blob/master/2022/day17/code.py). (I did not have time today)
+
+## Setup
+
+To run this code, you should get the input from [this link](https://adventofcode.com/2022/day/17/input) and paste it into `jet-pattern.txt` but my input is already provided as a sample. You should also make sure to have Python installed (I use Python 3.10.7 but it should work with most versions).
+
+# Run The Code
+
+```bash
+python3 pyroclastic-flow.py
+```
---
+++ 2022/(copied) day-17/pyroclastic-flow.py
@@ -0,0 +1,181 @@
+# from time import perf_counter
+import math
+from time import time as perf_counter
+from collections import defaultdict
+
+
+def profiler(method):
+    def wrapper_method(*arg, **kw):
+        t = perf_counter()
+        ret = method(*arg, **kw)
+        print(
+            "Method "
+            + method.__name__
+            + " took : "
+            + "{:2.5f}".format(perf_counter() - t)
+            + " sec"
+        )
+        return ret
+
+    return wrapper_method
+
+
+minus = [list("####")]
+plus = [
+    list(".#."),
+    list("###"),
+    list(".#."),
+]
+
+l = [
+    list("..#"),
+    list("..#"),
+    list("###"),
+][::-1]
+
+eye = [["#"], ["#"], ["#"], ["#"]]
+
+square = [list("##"), list("##")]
+
+shapes = [minus, plus, l, eye, square]
+
+
+class grid:
+    def __init__(self, pattern):
+        self.data = set()
+        self.top = 0
+        self.current_shape_idx = 0
+        self.shape_x = 2
+        self.shape_y = 3
+        self.settled = 0
+        self.pattern = pattern
+        self.pat_idx = 0
+
+    def can_move_horizontal(self, direction):
+        if direction == "<":
+            nx = self.shape_x - 1
+        else:
+            nx = self.shape_x + 1
+
+        if nx in [-1, 7]:
+            return False
+
+        for y, l in enumerate(shapes[self.current_shape_idx]):
+            for x, c in enumerate(l):
+                if c == "#":
+                    if (nx + x, self.shape_y + y) in self.data or nx + x > 6:
+                        return False
+        return True
+
+    def can_move_down(self):
+        ny = self.shape_y - 1
+
+        if ny < 0:
+            return False
+
+        for y, l in enumerate(shapes[self.current_shape_idx]):
+            for x, c in enumerate(l):
+                if c == "#":
+                    if (self.shape_x + x, ny + y) in self.data:
+                        return False
+        return True
+
+    def move(self):
+        direction = self.pattern[self.pat_idx % len(self.pattern)]
+        self.pat_idx = self.pat_idx + 1
+
+        if self.can_move_horizontal(direction):
+            if direction == "<":
+                self.shape_x -= 1
+            else:
+                self.shape_x += 1
+
+        if self.can_move_down():
+            self.shape_y -= 1
+        else:
+            return True
+
+        return False
+
+    def drop(self):
+        while True:
+            ret = self.move()
+            if ret:
+                break
+
+        self.draw()
+
+    def get_top(self):
+        return max(c[1] for c in self.data)
+
+    def draw(self):
+        for y, l in enumerate(shapes[self.current_shape_idx]):
+            for x, c in enumerate(l):
+                if c == "#":
+                    self.data.add((self.shape_x + x, self.shape_y + y))
+
+        self.settled += 1
+        self.top = self.get_top()
+        self.current_shape_idx = (self.current_shape_idx + 1) % 5
+        self.shape_x = 2
+        self.shape_y = self.top + 3 + 1
+
+    def plot(self):
+        buff = []
+        for y in range(100):
+            st = ""
+            for x in range(7):
+                st += "#" if (x, y) in self.data else "."
+            buff.append(st)
+
+        for l in buff[::-1]:
+            print(l)
+
+    def find_pattern(self):
+        states = defaultdict(list)
+        heights = {}
+
+        while True:
+            self.drop()
+            top = self.get_top()
+
+            heights[self.settled] = top + 1
+
+            if all((x, top) in self.data for x in range(7)):
+                s = (self.pat_idx % len(self.pattern), self.current_shape_idx)
+                states[s].append(self.settled)
+                if len(states[s]) == 2:
+                    return states[s], heights
+
+
+@profiler
+def part1():
+    g = grid(open("jet-pattern.txt").read())
+
+    for _ in range(2022):
+        g.drop()
+
+    print(g.get_top() + 1)
+
+
+@profiler
+def part2():
+    g = grid(open("jet-pattern.txt").read())
+
+    pt, heights = g.find_pattern()
+
+    extra_rocks = (1000000000000 - max(pt)) % (max(pt) - min(pt))
+    extra_height = heights[min(pt) + extra_rocks] - heights[min(pt)]
+
+    cycles = (1000000000000 - min(pt)) // (max(pt) - min(pt))
+    cycle_height = heights[max(pt)] - heights[min(pt)]
+
+    initial_height = heights[min(pt)]
+
+    print(initial_height + cycles * cycle_height + extra_height)
+
+
+if __name__ == "__main__":
+
+    part1()
+    part2()
