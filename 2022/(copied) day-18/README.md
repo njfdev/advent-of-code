54760787eae32255817681e282b5bc713bb377ca
Nicholas Fasching <91689117+njfdev@users.noreply.github.com>
2022-12-18 10:53:15 -0500

    Add Day 18 for 2022

---
+++ 2022/(copied) day-18/README.md
@@ -0,0 +1,15 @@
+# Day 18 - Advent of Code
+
+You can find the link for day 18, 2022 [here](https://adventofcode.com/2022/day/18). This folder will provide complete solutions for part 1 and part 2 of day 18.
+
+NOTE: This code was used from Reddit User [RadioactiveHop](https://topaz.github.io/paste/#XQAAAQAlCgAAAAAAAAARiEJHiiMzw3cPM/1Vl+2nx/DqKkM2x5f88ON9vJXaJJauvYrMh3t2GVQq+Oe+cCl2OJXEc7ExbySStf89yHEuTQMzz51IigUn6fs4RcMa+4MdMooE0Tq335fNlExE4zKDTVd3HQiySYTkgNNuJWl7Id2sXqufxERbkj1Ruopu+evP2GixEflcbfQHL95qahwN+RFh9j97OtstA0f1hOuGlSL02l5I+SFih3Isu1mHDIm8b4k2u7+KNsh0vrWZ1/LVu13aXiCIdiFzSNgnS8/5LeF9JufnROcu2tV7eiFbz5o5bLGW1y9rV0X3Ir8sUNtijebPoIgd3LfSkSbWAJSlgJQ5TWtq4FZBIVbE48Fmy+EtrcnfFgcUsMbvWSGfdGhulwovxm1PoFajutBAs3SUXsb+jjZJqrmJ1srNuR34spQMDviSeqJnkWeTC/+caFktbrkJzaTXgNI+MHCtJMYC8ERbCigSfWN77zgJe+Fsq/nRosqzwP2T39rT1wT8WVYVoaLczMFfMFRlNWlPv2OZCEFTeJv7PjUtNxdWb7vu0upSJV4LePs9X1pHH3FdY1gaSl5sDh1ixVx5yVphdeq8LR39KcTXQvANrqb9cR5JzQxOqNq85VV2YaxemOD8bWd7zXTGjV6UKMDY3uF60M62ZybVe9SZsuRWQMsbAxHY4EVim4WEBt1ahV5QjaUHH7ut8v3Dvz0CUbE5TFM2eXuLVkMqxfNitM0jrotoBLVTrPXt+hP6DFRnr7887LY9RRRRHd87uZLE6zP504H1JuDc4qdgqdpR4omzgFBiX+0glBzsT0HBLfNdV6TzdvP3Z/7zOIxcXtqkiDBShKorCkeQuc+mcmxrMKzQSviMZZaZLv8A9MAWAPOS8xgJxIef3J9a3zKdut68t84iit9FJs5LpezOVFEwUw/ZYVWRroGquG6buCzx+N4F3fssvB/kGVL6qj1AVPTy9Oki5R2eDBJ4ddrk57CVy83oJCngPfEMjKCYh+Q9LN39a6WUTEhi4jKfKSEsuW1E4pPYkOiZ2V7Kgr4iSZV8wFcbvRqZF/9MFZCXVQYEXsXd8Y/GCsVfyUjHuooENAJA075yviO7WI7Dv7S0Y/+t79/OaSvt+QNm/+F0xoXc9SDbdX4ygywzYIS7o6z505q8GfxGxWjFrMRH/EI7gRECnIR0IuyL025kMVDM/rpp27DiAMDLUqVR4y3ikyAhShqZyipRHK/B29TApGkF4XC/zStJcTQebEPPVDUiPoJS35pAYYKhuIFYwdh1N0mm2s1BC3StbK4cXYgP2a/lwf5D4n1wOqp3ltrqjkXFSYfW7yh+Mu0PlkbaDxe6Hzio8Jb6q0RlJUH/XEX7oOan). (I did not have time today)
+
+## Setup
+
+To run this code, you should get the input from [this link](https://adventofcode.com/2022/day/18/input) and paste it into `droplet-data.txt` but my input is already provided as a sample. You should also make sure to have Python installed (I use Python 3.10.7 but it should work with most versions).
+
+# Run The Code
+
+```bash
+python3 boiling-boulders.py
+```
---
+++ 2022/(copied) day-18/boiling-boulders.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from time import time
+
+input_file = "droplet-data.txt"
+
+origin = (0, 0, 0)
+dirs = [(1, 0, 0), (-1, 0, 0), (0, 1, 0), (0, -1, 0), (0, 0, 1), (0, 0, -1)]
+
+
+def manhattan(a, b):
+    # manhattan distance between a & b
+    return sum(map(lambda c: abs(c[0] - c[1]), zip(a, b)))
+
+
+def dist(a, b):
+    # Euclidean distance
+    return ((a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2 + (a[2] - b[2]) ** 2) ** 0.5
+
+
+def steam_path(a, b, droplet, visited=None):
+    # A* - return length of path from a to b
+    if visited is None:
+        visited = set()
+    visited.add(a)
+    adj = [tuple(map(sum, zip(a, d))) for d in dirs]
+    if b in adj:
+        # b is directly reachable
+        return 1, visited
+    candidate = [(a, manhattan(a, origin)) for a in adj]
+    for n, d in sorted(candidate, key=lambda a: a[1]):
+        if n in droplet:
+            # solid cube
+            continue
+        if n in visited:
+            # already checked
+            continue
+        p, v = steam_path(n, b, droplet, visited)
+        if p is not None:
+            visited.update(v)
+            return (p + 1), visited
+    return None, visited
+
+
+if __name__ == "__main__":
+    start_time = time()
+    cubes = dict.fromkeys(
+        [
+            tuple(map(int, l.strip().split(",")))
+            for l in open(input_file, "r").readlines()
+        ],
+        None,
+    )
+    pockets = set()
+    for xyz in cubes.keys():
+        if cubes[xyz] is None:
+            # first counter tracks faces exposed to another solid block
+            # second is faces exposed to air in a pocket
+            cubes[xyz] = list(0 for _ in range(2))
+        for d in dirs:
+            adj = tuple(map(sum, zip(xyz, d)))
+            if adj in cubes:
+                # solid adjacent cube
+                cubes[xyz][0] += 1
+            # part 1 ends here...
+            else:
+                # check if pocket
+                if adj in pockets:
+                    cubes[xyz][1] += 1
+                else:
+                    # we assume origin is outside of droplet
+                    l, v = steam_path(adj, origin, cubes)
+                    if l is None:
+                        # found a pocket
+                        # add all visited cells to cache
+                        pockets.update(v)
+                        cubes[xyz][1] += 1
+
+    print("Part #1 :", len(cubes) * 6 - sum(v[0] for v in cubes.values()))
+
+    print(
+        "Part #2 :",
+        len(cubes) * 6
+        - sum(v[0] for v in cubes.values())
+        - sum(v[1] for v in cubes.values()),
+    )
+
+    print("Execution time: {:.6f}s".format((time() - start_time)))
