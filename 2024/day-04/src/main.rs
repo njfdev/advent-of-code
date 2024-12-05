101580f89e772c6194494381808892c1970f1387
Nicholas Fasching <91689117+njfdev@users.noreply.github.com>
2024-12-05 15:47:06 -0500

    Add Part 2 for Day-04 for 2024

--- 2024/day-04/src/main.rs
+++ 2024/day-04/src/main.rs
@@ -65,6 +65,47 @@ fn part1(wordsearch: &Vec<String>, word: &String) -> u64 {
     return total;
 }
 
+fn does_2d_match(correct: &String, found: Vec<String>) -> bool {
+    // get diagonals
+    let down_right = found
+        .clone()
+        .iter()
+        .enumerate()
+        .map(|(i, val)| val.get(i..(i + 1)).unwrap())
+        .collect::<String>();
+
+    let down_left = found
+        .clone()
+        .iter()
+        .enumerate()
+        .map(|(i, val)| val.get((val.len() - i - 1)..(val.len() - i)).unwrap())
+        .collect::<String>();
+
+    return (correct == &down_right || correct == &(down_right.chars().rev().collect::<String>()))
+        && (correct == &down_left || correct == &(down_left.chars().rev().collect::<String>()));
+}
+
+fn part2(wordsearch: &Vec<String>, word: &String) -> u64 {
+    let mut total = 0;
+
+    // find all groups of 3
+    for r in 0..(wordsearch.len() - word.len() + 1) {
+        for c in 0..(wordsearch[r].len() - word.len() + 1) {
+            let mut section: Vec<String> = vec![];
+
+            for i in 0..word.len() {
+                section.push(wordsearch[r + i][(c)..(c + word.len())].to_string());
+            }
+
+            if does_2d_match(&word, section) {
+                total += 1;
+            }
+        }
+    }
+
+    return total;
+}
+
 fn main() {
     let contents =
         fs::read_to_string("./src/input.txt").expect("Should be able to read input file");
@@ -78,5 +119,8 @@ fn main() {
         "Total XMASs (Part 1): {}",
         part1(&lines, &("XMAS".to_string()))
     );
-    //println!("Total Multiplication Value (Part 2): {}", part2(&contents));
+    println!(
+        "Total X-MASs (Part 2): {}",
+        part2(&lines, &("MAS".to_string()))
+    );
 }
