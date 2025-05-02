2dacd8afc7e1aa4547332ef76ccf34a35a4a74ae
Nicholas Fasching <91689117+njfdev@users.noreply.github.com>
2025-05-01 20:11:13 -0400

    Add Part 1 of Day 02 for 2015

---
+++ 2015/day-02/Program.java
@@ -0,0 +1,87 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class Program {
+
+  public static String inputPath = "./input.txt";
+
+  public static void main(String[] args) {
+
+      ArrayList<String> input = getInput();
+
+      int totalWrappingPaper = 0;
+
+      for (String dimension : input) {
+        totalWrappingPaper += getNeededWrappingPaper(getDimensionsFromString(dimension));
+      }
+
+      System.out.println("The needed square feet of wrapping paper is: " + totalWrappingPaper);
+
+  }
+
+  public static int[] getDimensionsFromString(String sDim) {
+    String curSDim = sDim;
+    int[] dimensions = new int[3];
+
+    for (int i = 0; i < 3; i++) {
+      int stopIndex = curSDim.indexOf("x");
+
+      if (stopIndex == -1) {
+        stopIndex = curSDim.length();
+      }
+
+      dimensions[i] = Integer.parseInt(curSDim.substring(0, stopIndex));
+
+      if (i < 2) {
+        curSDim = curSDim.substring(stopIndex+1);
+      }
+    }
+
+    return dimensions;
+  }
+
+  public static int getNeededWrappingPaper(int[] dim) {
+    int[] faces = new int[3];
+    faces[0] = dim[0]*dim[1];
+    faces[1] = dim[0]*dim[2];
+    faces[2] = dim[1]*dim[2];
+
+    int smallestFace = faces[0];
+
+    for (int face : faces) {
+      if (face < smallestFace) {
+        smallestFace = face;
+      }
+    }
+
+    return 2*faces[0] + 2*faces[1] + 2*faces[2] + smallestFace;
+  }
+
+  public static ArrayList<String> getInput() {
+
+    try {
+
+      File file = new File(inputPath);
+      Scanner reader = new Scanner(file);
+
+      ArrayList<String> input = new ArrayList<String>();
+
+      while (reader.hasNextLine()) {
+        input.add(reader.nextLine());
+      }
+
+      reader.close();
+
+      return input;
+
+    } catch (FileNotFoundException e) {
+      System.out.println("Could not find: " + inputPath);
+    }
+
+    return new ArrayList<String>();
+
+  }
+
+}
